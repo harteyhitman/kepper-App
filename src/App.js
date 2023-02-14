1587846f68108caf1f159e56db6a056538860104
harteyhitman <oyegoke.a18@gmail.com>
2023-02-14 13:27:31 +0100

    renew-keeperApp

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from 'react'
-import "./App.css"
-import Header from './Header'
 import Footer from './Footer'
+import Header from './Header'
 import Note from './Note'
+import './App.css'
+
 
 function App() {
-    return(
-        <div>
-            <Header />
-            <Note />
-            <Footer />
-        </div>
-        
-    )
+  return (
+    <div>
+        <Header />
+        <Note />
+        <Footer />
+    </div>
+  )
 }
 
 export default App
